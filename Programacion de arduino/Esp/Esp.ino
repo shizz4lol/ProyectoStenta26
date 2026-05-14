997665b3eb6f12d35077bb6e37e72e16e8b95bc0
Alí <perezali825@gmail.com>
2026-05-14 18:01:27 -0300

    Add files via upload
    Actualizacion del codigo anterior

--- Programacion de arduino/Esp/Esp.ino
+++ Programacion de arduino/Esp/Esp.ino
@@ -1,38 +1,62 @@
-//Libreras que se incluyen
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-// Pin donde conecta el sensor
 #define PIN_SENSOR 21
 
-// Configuración de comunicación
 OneWire oneWire(PIN_SENSOR);
 DallasTemperature sensores(&oneWire);
+
+const char* ssid = "Cuando tenga el Esp32";
+const char* password = "Cuando tenga el Esp32";
+
+const char* servidor = "https://tuweb.infinityfreeapp.com/php)";
 
 void setup() {
   Serial.begin(115200);
   sensores.begin();
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando");
+  }
+
+  Serial.println("Conectado");
 }
 
 void loop() {
 
-  // Se pide temperatura al sensor
   sensores.requestTemperatures();
 
-  // Se obtiene la temperatura del sensor
   float temperatura = sensores.getTempCByIndex(0);
 
-  // Se verifica si hay algun error
   if (temperatura == DEVICE_DISCONNECTED_C) {
     Serial.println("Error");
     return;
   }
 
-  // Se muestra los datos en pantalla
   Serial.print("Temperatura: ");
-  Serial.print(temperatura);
-  Serial.println(" °C");
+  Serial.println(temperatura);
 
+  if (WiFi.status() == WL_CONNECTED) { //Envio de datos al ervidor
+
+    HTTPClient http;
+
+    http.begin(servidor);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    String datos = "temperatura=" + String(temperatura);
+
+    int codigo = http.POST(datos);
+
+    Serial.print("Respuesta: ");
+    Serial.println(codigo);
+
+    http.end();
+  }
 
   delay(3000);
 }
